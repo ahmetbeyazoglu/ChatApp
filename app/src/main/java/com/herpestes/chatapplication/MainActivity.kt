e9244c14d2d5110c6d0948735807f54cea4370fc
herpestes <ahmetbeyazoglu@gmail.com>
2023-06-06 23:57:22 +0300

    Snapshot

--- app/src/main/java/com/herpestes/chatapplication/MainActivity.kt
+++ app/src/main/java/com/herpestes/chatapplication/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.herpestes.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Contacts.Data
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,14 +17,39 @@ class MainActivity : AppCompatActivity() {
     private lateinit var userList: ArrayList<User>
     private lateinit var adapter: UserAdapter
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mAuth = FirebaseAuth.getInstance()
+        mDbRef= FirebaseDatabase.getInstance().getReference()
 
         userList = ArrayList()
         adapter = UserAdapter(this,userList)
+
+        userRecyclerView = findViewById(R.id.userRecyclerView)
+
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+        userRecyclerView.adapter = adapter
+
+        mDbRef.child("user").addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                userList.clear()
+                for(postSnapchot in snapshot.children){
+                    val currentUser = postSnapchot.getValue(User::class.java)
+                    userList.add(currentUser!!)
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -33,7 +62,9 @@ class MainActivity : AppCompatActivity() {
         if(item.itemId == R.id.logout){
             //write the login for logout
             mAuth.signOut()
+            val intent = Intent(this@MainActivity,Login::class.java)
             finish()
+            startActivity(intent)
             return true
         }
         return true
